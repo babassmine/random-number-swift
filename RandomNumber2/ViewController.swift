11214194c35141a8eec3b62aed9ef812af1ebd74
Abass Sesay <bassco10@gmail.com>
2015-11-14 13:12:56 -0500

    Initial Commit

---
+++ RandomNumber2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomNumber2
+//
+//  Created by Abass Sesay on 11/14/15.
+//  Copyright © 2015 Abass Sesay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
